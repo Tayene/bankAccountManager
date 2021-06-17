acd36e4162f627781d5aba795c287985c2017d4b
Goncalo <tayene.carla.goncalo@accenture.com>
2021-06-17 15:14:05 -0300

    atualizando arquivos de rotas

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,14 @@ const DepositBankController = require('./controller/bankDepositController');
 
 const routes = express.Router();
  
-routes.get('/user', UserController.findUser);
+routes.get('/user/findByPk', UserController.findUser);
+routes.get('/user', UserController.findAll);
 routes.post('/user', UserController.createUser);
 routes.put('/user', UserController.updateUser);
 routes.delete('/user', UserController.deleteUser);
 
-routes.get('/user/account', AccountController.findAccountByPk);
+routes.get('/user/account/findByPk', AccountController.findAccountByPk);
+routes.get('/user/account', AccountController.findAll);
 routes.post('/user/account', AccountController.createUserAccount);
 routes.put('/user/account', AccountController.updateUserAccount);
 routes.delete('/user/account', AccountController.deleteUserAccount);
@@ -22,5 +24,7 @@ routes.get('/user/transfer/findByPk', BankTransferController.findTransferByPk);
 routes.post('/user/transfer', BankTransferController.createTransfer);
 
 routes.post('/user/deposit', DepositBankController.createDeposit);
+routes.get('/user/deposit', DepositBankController.findAll);
+routes.get('/user/deposit/findByPk', DepositBankController.findDepositByPk);
 
 module.exports = routes;
